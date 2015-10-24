9983cb380e7b9e80672b9220585cd064cd0a348f
Alex Lynham <acelynham@hotmail.com>
2015-10-24 18:04:50 +0100

    Basic values passed into template.

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -3,7 +3,7 @@ class CompositionsController < ApplicationController
   def show
     text_to_use = Text.latest
 
-    @overall_probability = text_to_use.analyse_sentiment
+    @sentiment = text_to_use.analyse_sentiment
     @content = text_to_use.content
     @sender = text_to_use.sender
   end
