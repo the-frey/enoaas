6816e949c23c6bc8fa81065199a5e0a443462805
Joseph Anderson <josepand@cisco.com>
2015-10-24 21:58:48 +0100

    Use history.

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -3,10 +3,10 @@ class CompositionsController < ApplicationController
   def show
     text_to_use = Text.latest
 
-    @sentiment = text_to_use.analyse_sentiment
-    @content_length = text_to_use.content.length
-    @chord_progression = text_to_use.chord_progression
-    @tempo = text_to_use.tempo
+    @sentiment = Text.analyse_sentiment_history
+    @content_length = Text.length_history
+    @chord_progression = Text.chord_progression_history
+    @tempo = Text.tempo_history
   end
 
 end
