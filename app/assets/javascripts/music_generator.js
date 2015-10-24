5fbe4e5ffca397c2c16d025777799a4f22d0348c
jonnydark <jonny@jonnydark.com>
2015-10-24 18:03:30 +0100

    Tidied up some of the stuff in the music generator

--- app/assets/javascripts/music_generator.js
+++ app/assets/javascripts/music_generator.js
@@ -1,23 +1,19 @@
-$(function(){
+function setupGibber() {
+  window.eno = window.eno || new Object();
 
-    window.setupGibber = function() {
-      window.eno = window.eno || new Object();
+  window.eno.drums = EDrums('x*o*x*o-')
+  window.eno.drums.amp = .75
 
-      //Gibber.init(); // REQUIRED!
+  window.eno.bass = FM('bass')
+    .note.seq( [0,0,0,7,14,13].rnd(), [1/8,1/16].rnd(1/16,2) )
 
-      drums = EDrums('x*o*x*o-')
-        drums.amp = .75
+  window.eno.rhodes = Synth( 'rhodes', {amp:.35} )
+    .chord.seq( Rndi(0,6,3), 1 )
+    .fx.add( Delay() )
 
-      bass = FM('bass')
-        .note.seq( [0,0,0,7,14,13].rnd(), [1/8,1/16].rnd(1/16,2) )
+  Gibber.scale.root.seq( ['c4','ab3','bb3'], [4,2,2] )
+  Gibber.scale.mode.seq( ['Minor','Mixolydian'], [6,2] )
 
-      rhodes = Synth( 'rhodes', {amp:.35} )
-        .chord.seq( Rndi(0,6,3), 1 )
-        .fx.add( Delay() )
+};
 
-      Gibber.scale.root.seq( ['c4','ab3','bb3'], [4,2,2] )
-      Gibber.scale.mode.seq( ['Minor','Mixolydian'], [6,2] )
 
-      window.eno.drums = drums;
-    };
-});
