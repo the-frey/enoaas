c0b8d54b117b64276dbf6178ac9c9a12ef3d988f
Alex Lynham <acelynham@hotmail.com>
2015-10-25 11:10:49 +0000

    Vintage syntax error.

--- app/controllers/concerns/clockwork_api.rb
+++ app/controllers/concerns/clockwork_api.rb
@@ -2,7 +2,7 @@ require 'clockwork'
 
 module ClockworkAPI
 
-  self.send_text(number, message)
+  def self.send_text(number, message)
     api = Clockwork::API.new(ENV['CLOCKWORK_API_KEY'])
     message = api.messages.build(:to => number, :content => message)
     response = message.deliver
